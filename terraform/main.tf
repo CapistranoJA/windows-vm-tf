095f55f13e10f71b904b748611c6c7bbaa83f00d
CapistranoJA <capistranojp000@gmail.com>
2026-08-08 19:03:44 +0800

    libvirt_pool created

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
 provider "libvirt" {
   uri = var.libvirt_uri
 }
+
+
+resource "libvirt_pool" "windows" {
+  name = var.pool_name
+  type = "dir"
+  target = {
+    path = var.pool_path
+  }
+  
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,15 @@ variable "libvirt_uri" {
   type = string
   default = "qemu:///system"
 }
+
+variable "pool_name" {
+  description = "Libvirt storage pool name"
+  type = string
+  default = "windows-11"
+}
+
+variable pool_path {
+  description = "Libvirt storage pool path"
+  type = string
+  default = "/data/lib/libvirt/images/windows-11"
+}
